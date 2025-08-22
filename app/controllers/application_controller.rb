6fef64f8b797f2b610f9451ee052dd5c1f8261ca
Marcel <marcel.hasinski@proxi.cloud>
2025-08-22 15:11:49 +0200

    Setup chat for gpt 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,21 @@
 class ApplicationController < ActionController::Base
   include Authentication
 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :require_authentication
 
   def index; end
+
+  def create
+    return if params[:message].blank?
+
+    @chat_mode = params[:chat_mode]
+    @message = params[:message]
+    @reply = OpenAiClient.new.chat(@message, @chat_mode)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
+  end
 end
--- app/services/open_ai_client.rb
+++ app/services/open_ai_client.rb
@@ -1,21 +1,17 @@
 require 'openai'
 
 class OpenAiClient
-
   def initialize
     @client = OpenAI::Client.new
   end
 
-  def chat(message)
-    @client.chat(
+  def chat(message, chat_mode)
+    @client.responses.create(
       parameters: {
-        model: "gpt-4o",
-        messages: [{ role: "user", content: message}],
-        temperature: 0.7,
-        stream: proc do |chunk, _event|
-          print chunk.dig("choices", 0, "delta", "content")
-        end
+        model: chat_mode,
+        input: message,
+        reasoning: { effort: 'minimal' }
       }
-    )
+    ).dig('output', 1, 'content', 0, 'text')
   end
 end
