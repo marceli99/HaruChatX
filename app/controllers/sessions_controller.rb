16b65967e1f60f7d6e80fbcea7e83ea2104de638
Marcel <marcel.hasinski@proxi.cloud>
2025-08-24 15:27:59 +0200

    Add translations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,15 @@ class SessionsController < ApplicationController
   def create
     if (user = User.authenticate_by(email_address: params[:email_address], password: params[:password]))
       start_new_session_for(user)
-      redirect_to after_authentication_url, notice: 'Signed in successfully'
+      redirect_to after_authentication_url, notice: t('flash.sessions.create.success')
     else
-      flash.now[:alert] = 'Invalid email or password'
+      flash.now[:alert] = t('flash.sessions.create.failure')
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     terminate_session
-    redirect_to new_session_url, notice: 'Signed out'
+    redirect_to new_session_url, notice: t('flash.sessions.destroy.success')
   end
 end
