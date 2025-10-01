965603dae6b895de75a360853a0cde3cdd624ba6
Marcel <marcel.hasinski@proxi.cloud>
2025-10-01 16:11:00 +0200

    Update theme

--- app/services/open_ai_client.rb
+++ app/services/open_ai_client.rb
@@ -12,7 +12,7 @@ class OpenAiClient
       parameters: {
         model: chat_mode,
         input: message,
-        # system_prompt: system_prompt,
+        instructions: 'Jesteś miłym asystentem AI, odpowiadaj na pytanie w języku pytania. Jeśli nie znasz odpowiedzi, nie zmyślaj jej. Odpowiadaj krótko i na temat. Jeśli twoja odpowiedź zawiera kod programistyczny, umieść go w znaczniku <pre><code></code></pre>.',
         reasoning: { effort: 'minimal' }
       }
     ).dig('output', 1, 'content', 0, 'text')
