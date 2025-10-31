97031139395ed82b376c42910f7a9b69c89e10f8
Kanewersa <kanewers@gmail.com>
2025-10-31 18:49:57 +0100

    Update config

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -2,8 +2,14 @@
 
 RubyLLM.configure do |config|
   config.openai_api_key = Rails.application.credentials.dig(:openai, :access_key)
-  # config.default_model = "gpt-4.1-nano"
+  config.anthropic_api_key = Rails.application.credentials.dig(:anthropic, :access_key)
+  config.gemini_api_key = Rails.application.credentials.dig(:gemini, :access_key)
+  config.vertexai_project_id = Rails.application.credentials.dig(:google_cloud, :project_id)
+  config.vertexai_location = Rails.application.credentials.dig(:google_cloud, :location)
+  config.deepseek_api_key = Rails.application.credentials.dig(:deepseek, :access_key)
+  config.mistral_api_key = Rails.application.credentials.dig(:mistral, :access_key)
+  config.perplexity_api_key = Rails.application.credentials.dig(:perplexity, :access_key)
+  config.openrouter_api_key = Rails.application.credentials.dig(:openrouter, :access_key)
 
-  # Use the new association-based acts_as API (recommended)
   config.use_new_acts_as = true
 end
